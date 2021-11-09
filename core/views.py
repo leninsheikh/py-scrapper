2a3db747d1826a59a226022d1bbb32df48f67a49
Lenin Sheikh <justtlenin@gmail.com>
2021-11-09 21:36:10 +0600

    basic web scrapper example added

--- core/views.py
+++ core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
+import logging
+import requests
+from bs4 import BeautifulSoup
 from .models import Shop, Product
+from django.utils import timezone
 
 def index(req):
     products = Product.objects.all()
@@ -10,9 +14,23 @@ def index(req):
     return render(req, 'core/index.html', context)
 
 def load(req):
+    page = requests.get("https://www.startech.com.bd/laptop-notebook/laptop?limit=1000")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    items = soup.select('.p-item')
 
+    shop = Shop.objects.get(id=1)
+    products = []
+
+    for item in items:
+        products.append(Product(
+            type='laptop',
+            shop=shop,
+            name=item.select_one('.p-item-name').get_text(),
+            price=item.select_one('.p-item-price').get_text())
+        )
+    
+    Product.objects.bulk_create(products)
 
     return redirect('/core')
-
 
 
